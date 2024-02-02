046057362956d286cab199c11f14213fb9c4ae71
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-02-02 11:01:37 -0800

    added package management, treesitter, and a base colorscheme

--- init.lua
+++ init.lua
@@ -1,16 +1,29 @@
 --[ OPTIONS ]--
+
+-- global options
 local g = vim.g -- global vars and options
 g.mapleader = " " -- sets starting key for custom keybinds
 
 local opt = vim.opt -- ?? how different from vim.g?
 
-opt.termguicolors = true -- enabled coloring
+-- current line behavior
 opt.cursorline = true -- highlights the current line
 opt.number = true -- sets line numbers
-opt.background = "dark"
 opt.relativenumber = true -- sets line numbering as relative to current line
 
+-- colors
+opt.termguicolors = true -- enabled coloring
+opt.background = "dark"
+local colorscheme = "slate"
+local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not ok then
+  print("colorscheme " .. colorscheme .. " was not found!")
+  return
+end
+
 --[ PLUGINS ]--
+
+-- lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -23,5 +36,35 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
---require("lazy").setup(plugins, opts)
+
+-- treesitter
+require("lazy").setup({{
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      -- list of parsers to always have instlaled, the first 5 are required
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "rust", "java" },
+      
+      -- install the above ensured parsers synchronously
+      sync_install = false,
+
+      highlight = {
+        enable = true,
+        --[[ -- disables syntax highlighting for overly large files
+        disable = function(lang, buf)
+          local maxFilesize = 100 * 1024 -- 100 KiB
+	  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats > maxFilesize then
+	    return true
+          end
+        end,
+        ]]--
+        additional_vim_regex_highlighting = false
+      }
+    })
+  end
+}})
 
