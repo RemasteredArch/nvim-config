bb0395c21059b7daaf009fcacb823f7ba753e91b
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-06-25 16:56:21 -0700

    Added shell script-specific configs

---
+++ ftplugin/sh.lua
@@ -0,0 +1,23 @@
+--[[
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+Copyright © 2024 RemasteredArch
+
+This file is part of nvim-config.
+
+nvim-config is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+nvim-config is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with nvim-config. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+-- sh.lua: configuration for writing shell scripts
+
+local current_file = vim.fn.expand("%")
+
+vim.keymap.set("n", "<leader>r", "<cmd>split | term ./" .. current_file .. "<cr>")
+vim.keymap.set("n", "<leader>cr", function()
+  local user_input = vim.fn.input("Args: ")
+  vim.api.nvim_command("split | term ./" .. current_file .. " " .. user_input)
+end)
