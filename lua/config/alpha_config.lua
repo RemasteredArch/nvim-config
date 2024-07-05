9117dab59ac9b16f5846c2a4ac025f090a76d221
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-05 14:17:11 -0700

    Startup screen: Fixed issue where the custom buffer name broke opening files

--- lua/config/alpha_config.lua
+++ lua/config/alpha_config.lua
@@ -65,6 +65,8 @@ dashboard.section.footer.val = {
   os.date(" %A, %Y-%m-%d   %I:%M %p")
 }
 
-vim.api.nvim_buf_set_name(0, "<3") -- Set current buffer's name (default: [Scratch])
+dashboard.config.opts.setup = function()
+  vim.api.nvim_buf_set_name(0, "<3") -- Set current buffer's name (default: [Scratch])
+end
 
 require("alpha").setup(dashboard.config)
