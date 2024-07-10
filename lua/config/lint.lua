64e0a7f69ed087d573dbd56cfd2ac5a36036f00e
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-10 16:57:00 -0700

    lint config: Removed vestigial code

--- lua/config/lint.lua
+++ lua/config/lint.lua
@@ -19,11 +19,6 @@ local module = {}
 function module.setup(linters_by_ft)
   require("lint").linters_by_ft = linters_by_ft
 
-  local filetypes_with_linters = {}
-  for filetype in pairs(linters_by_ft) do
-    table.insert(filetypes_with_linters, filetype)
-  end
-
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
       require("lint").try_lint() -- Runs based on linters_by_ft
