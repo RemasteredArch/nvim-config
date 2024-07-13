45721fb26f403f7c0378ea29f565c3a8f33eb218
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-12 22:37:23 -0700

    startup screen: changed Mason's icon from a hammer to a download icon

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -59,7 +59,7 @@ dashboard.section.buttons.val = {
   dashboard.button("i", "  > New file", ":enew <BAR> startinsert <CR>"),
   dashboard.button("e", "  > Open Netrw", ":Explore <CR>"),
   dashboard.button("l", "󰒲  > Open Lazy", ":Lazy <CR>"),
-  dashboard.button("m", "󰣪  > Open Mason", ":Mason <CR>"),
+  dashboard.button("m", "  > Open Mason", ":Mason <CR>"),
   dashboard.button("q", "󰈆  > Quit Neovim", ":qa <CR>")
 }
 
