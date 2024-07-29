62091512101e96f3fd9cd0356b7597c4e4ba7a4f
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-28 22:25:49 -0600

    keymap.lua: documentation comments, fixes, and changes

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -14,31 +14,62 @@ You should have received a copy of the GNU Affero General Public License along w
 
 -- keymap.lua: various key mappings
 
+--- @alias command string A Neovim Ex command
+---
+--- @alias mode string | string[] Vim mode(s) the keybind should be active in
+--- @alias key string The actual keybind
+--- @alias effect command | function The effect of the keybind
+--- @alias opts vim.keymap.set.Opts
+---
+--- @class (exact) keymap A Neovim keymap, similar to the parameters of `vim.keymap.set()`
+--- @field mode mode
+--- @field key key
+--- @field effect effect
+--- @field opts opts?
+---
+--- @alias keymap_tuple [mode, key, effect, opts?] A Neovim keymap represented as a tuple
+---
+--- @class mappings_and_setup
+--- @field mappings keymap_tuple[]
+--- @field setup fun()
+
 local module = {}
 
--- Sets a key mapping
-function module.set(mode, key, effect, opts)
-  vim.keymap.set(mode, key, effect, opts)
+--- Sets a key mapping.
+---
+--- Just a wrapper around `vim.keymap.set()`.
+---
+--- @param keymap keymap
+function module.set(keymap)
+  vim.keymap.set(keymap.mode, keymap.key, keymap.effect, keymap.opts)
 end
 
+--- Set a list of keymaps in a given buffer.
+---
+--- @param binds keymap_tuple[]
+--- @param buffnr integer?
 function module.set_all(binds, buffnr)
   local function expand_keymap(keymap)
-    local mode = keymap[1]
-    local key = keymap[2]
-    local effect = keymap[3]
-    local opts = keymap[4] or {}
+    local map = {
+      mode = keymap[1],
+      key = keymap[2],
+      effect = keymap[3],
+      opts = keymap[4] or {}
+    }
 
     if buffnr then
-      opts.buffer = buffnr
+      map.opts.buffer = buffnr
     end
 
-    module.set(mode, key, effect, opts)
+    module.set(map)
   end
 
   vim.tbl_map(expand_keymap, binds)
 end
 
--- Key mappings for nvim-cmp
+--- Key mappings for nvim-cmp.
+---
+--- @return table<key, cmp.Mapping>
 function module.cmp()
   -- local cmp_action = require("lsp-zero").cmp_action() -- A few helper actions
   local cmp = require("cmp")
@@ -51,6 +82,11 @@ function module.cmp()
   })
 end
 
+--- Key mappings for Java.
+---
+--- @param buffnr integer
+--- @param root_files path[]
+--- @return mappings_and_setup
 function module.java(buffnr, root_files)
   local mappings = {
     -- In normal mode, press alt+o[rganize] to organize imports
@@ -58,14 +94,14 @@ function module.java(buffnr, root_files)
 
     -- In normal and visual mode mode, press c,r[efactor],v[ariable] to extract a variable
     { "n", "crv",        function() require("jdtls").extract_variable() end },
-    { "x", "crv",        function() require("jdtls").extract_variable(true) end },
+    { "x", "crv",        function() require("jdtls").extract_variable({ visual = true }) end },
 
     -- In normal and visual mode, press c,r[efactor],c[onstant] to extract a constant
     { "n", "crc",        function() require("jdtls").extract_constant() end },
-    { "x", "crc",        function() require("jdtls").extract_constant(true) end },
+    { "x", "crc",        function() require("jdtls").extract_constant({ visual = true }) end },
 
     -- In visual mode, press c,r[efactor],m[ethod] to extract a method
-    { "x", "crm",        function() require("jdtls").extract_method(true) end },
+    { "x", "crm",        function() require("jdtls").extract_method({ visual = true }) end },
 
     -- In normal mode, press space,r[un] to run the single-file code in the current buffer (or c[onfig]r[un] to run with input)
     { "n", "<leader>r",  "<cmd>split | term java %<cr>" },
@@ -95,6 +131,9 @@ function module.java(buffnr, root_files)
   return { mapping = mappings, setup = setup }
 end
 
+--- Key mappings for Rust.
+---
+--- @return mappings_and_setup
 function module.rust()
   local mappings = {
     -- Expand diagnostics
