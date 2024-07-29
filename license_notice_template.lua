be9ac85c728d5e7c9be5be0f7a30de139f95e405
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-29 08:20:27 -0600

    license_notice_template.lua: formatted to 80 character width

--- license_notice_template.lua
+++ license_notice_template.lua
@@ -5,9 +5,15 @@ Copyright © 2024 RemasteredArch
 
 This file is part of nvim-config.
 
-nvim-config is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+nvim-config is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
-nvim-config is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+nvim-config is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License along with nvim-config. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License along
+with nvim-config. If not, see <https://www.gnu.org/licenses/>.
 ]]
