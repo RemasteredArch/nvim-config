b93f5de44d08372794987e9773720104cd0e435d
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-29 05:24:40 -0600

    codestyles/java/Astral.lua: changed to use config.options.spaces(4) instead of custom config code

--- lua/codestyles/java/Astral.lua
+++ lua/codestyles/java/Astral.lua
@@ -14,12 +14,4 @@ You should have received a copy of the GNU Affero General Public License along w
 
 -- Astral.lua: custom config to adhere to the code style of https://github.com/Jaxydog/Astral
 
-local opt = vim.opt
-
--- spaces, not tabs
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.smarttab = true
-
-opt.tabstop = 8     -- make it obvious when a tab char is used instead of spaces
-opt.softtabstop = 0 -- redundant, but set just in case init.lua changes
+require("config.options").spaces(4)
