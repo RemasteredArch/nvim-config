77731777290a5fda256f0105a87f529c4410d923
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2025-03-09 20:52:06 -0700

    refactor: replace `vim.loop` with `vim.uv`

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ local packages = require("config.packages")
 
 -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
